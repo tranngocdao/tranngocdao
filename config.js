27933bd2de401b345331226d302eaf4f3cbf921b
tranngocdao <94595983+tranngocdao@users.noreply.github.com>
2021-11-18 14:29:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'nghiêm túc đây này =)))',
+    introDesc: `Tao có điều này muốn nói với mày , mày phải trả lời thật nhé :))`,
+    btnIntro: 'OK',
+    title: 'Tao biết Mày thích tao phải không  ? 😙',
+    desc: 'mày không trả lời hoặc thoát ra, tức là mày thích tao đó nha :> ',
+    btnYes: 'Có, em thật sự thích anh 💖💖💖',
+    btnNo: '💔 Mày bị ảo tưởng à  💔 ',
+    question:'Lý do gì mà thích 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/PhungQuocDinh'
+}
